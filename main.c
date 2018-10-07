1a6984eb0465bda10e01efb40d2b183209e5c627
Mick K <salabon77mk@gmail.com>
2018-10-07 16:09:50 -0500

    Vars

--- main.c
+++ main.c
@@ -6,8 +6,14 @@
 #define BUFFER_SIZE 1024
 #endif
 
-int main(void){
 
+
+
+int main(void){
+	pthread_t reader, munch1, munch2, writer;
+	struct Queue *munch1Queue;
+	struct Queue *munch2Queue
+	struct Queue *writerQueue;
 	//when finished
 	PrintQueueStats();
 }
