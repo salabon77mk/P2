390a4bebff916dc370ce804d4064642c2031ce8d
Mykola Kusyy <mykola@rockhopper-05.cs.wisc.edu>
2018-10-08 16:31:13 -0500

    Fixed function decl in main

--- main.c
+++ main.c
@@ -6,40 +6,43 @@
 #define BUFFER_SIZE 1024
 #endif
 
+//TODO make struct to hold args for readStream?
 
-static void *firstMunch(struct Queue *que);
-static void *secondMunch(struct Queue *que);
-static void *writeOutput(struct Queue *que);
-static void *readStream();
+static void *firstMunch(void *queue);
+static void *secondMunch(void *queue);
+static void *writeOutput(void *queue);
+static void *readStream(void *stream);
 static int checkSizie();
 
 int main(void){
+	
 	pthread_t reader, munch1, munch2, writer;
 	struct Queue *munch1Q;
 	struct Queue *munch2Q;
 	struct Queue *writerQ;
 
 	//will have to edit this around, readStream should return an updated char array
-	pthread_create(&reader, NULL, readStream, &args);
-	pthread_create(&munch1, NULL, firstMunch, munch1Q);
-	pthread_create(&munch2, NULL, secondMunch, munch2Q);
-	pthread_create(&writer, NULL, writeOutput, writerQ);
+	int createReader = pthread_create(&reader, NULL, readStream, /*notsureyet*/);
+	int createMunch1 = pthread_create(&munch1, NULL, firstMunch, munch1Q);
+	int createMucnh2 = pthread_create(&munch2, NULL, secondMunch, munch2Q);
+	int createWriter = pthread_create(&writer, NULL, writeOutput, writerQ);
 
 	//when finished
 	PrintQueueStats();
 }
 
-void *readStream(){
+void *readStream(void * stream){
 }
 
-void *firstMunch(struct Queue *que){
-
-}
-
-void *secondMunch(struct Queue *que){
+void *firstMunch(void *queue){
 
 }
 
-void *writeOutput(struct Queue *que){
+void *secondMunch(void *queue){
 
+}
+
+void *writeOutput(void *queue){
+	char *printMe = DequeueString(que);
+	printf("%s\n", printMe);	
 }
