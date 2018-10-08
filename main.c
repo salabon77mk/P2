39e87d23e4ac136f1164d2d6ebf710a30c4742b3
Mykola Kusyy <mykola@rockhopper-05.cs.wisc.edu>
2018-10-08 16:48:04 -0500

    Set up pthread_joins

--- main.c
+++ main.c
@@ -17,15 +17,26 @@ static int checkSizie();
 int main(void){
 	
 	pthread_t reader, munch1, munch2, writer;
-	struct Queue *munch1Q;
-	struct Queue *munch2Q;
-	struct Queue *writerQ;
+	struct Queue *munch1Queue;
+	struct Queue *munch2Queue;
+	struct Queue *writerQueue;
 
 	//will have to edit this around, readStream should return an updated char array
 	int createReader = pthread_create(&reader, NULL, readStream, /*notsureyet*/);
-	int createMunch1 = pthread_create(&munch1, NULL, firstMunch, munch1Q);
-	int createMucnh2 = pthread_create(&munch2, NULL, secondMunch, munch2Q);
-	int createWriter = pthread_create(&writer, NULL, writeOutput, writerQ);
+	threadCreateCheck(createReader);
+	pthread_join(reader, /*stuff got passed into reader*/);
+
+	int createMunch1 = pthread_create(&munch1, NULL, firstMunch, munch1Queue);
+	threadCreateCheck(createMunch1);
+	pthread_join(munch1, (void **) munch1Queue);
+
+	int createMucnh2 = pthread_create(&munch2, NULL, secondMunch, munch2Queue);
+	threadCreateCheck(createMunch2);
+	pthread_join(munch2, (void **) munch2Queue);
+
+	int createWriter = pthread_create(&writer, NULL, writeOutput, writerQueue);
+	threadCreateCheck(createWriter);
+	pthread_join(writer, (void **) writerQueue);
 
 	//when finished
 	PrintQueueStats();
@@ -45,4 +56,10 @@ void *secondMunch(void *queue){
 void *writeOutput(void *queue){
 	char *printMe = DequeueString(que);
 	printf("%s\n", printMe);	
+}
+
+void threadCreateCheck(int val){
+
+
+
 }
